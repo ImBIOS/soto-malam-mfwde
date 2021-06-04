5e5f13c09de2165cbd00a642666af7e6acf7d605
ImBIOS <41441643+ImBIOS@users.noreply.github.com>
2021-06-04 16:26:49 +0000

    Deploying to gh-pages from @ ImBIOS/soto-malam-mfwde@acbbf8f4d91bd17a25cef7b189412f9fbc880c3d 🚀

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 var serviceWorkerOption = {
   "assets": [
-    "/bundle.js",
-    "/favicon.png",
-    "/manifest.json",
-    "/icons/icon-128x128.png",
-    "/icons/icon-144x144.png",
-    "/icons/icon-152x152.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-384x384.png",
-    "/icons/icon-512x512.png",
-    "/icons/icon-72x72.png",
-    "/icons/icon-96x96.png",
-    "/images/heros/hero-image_3.jpg",
-    "/index.html"
+    "./bundle.js",
+    "./favicon.png",
+    "./manifest.json",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./images/heros/hero-image_3.jpg",
+    "./index.html"
   ]
 };
         
