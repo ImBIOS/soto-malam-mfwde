d25872b0d8e36737f492321d4ecd6d9266967211
Imamuzzaki <imamuzzaki@gmail.com>
2021-06-04 23:09:48 +0700

    add scope and replace ./

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -2,7 +2,11 @@ import runtime from "serviceworker-webpack-plugin/lib/runtime";
 
 const swRegister = async () => {
   if ("serviceWorker" in navigator) {
-    await runtime.register();
+    await runtime.register({ scope: "./" }).then((reg) => {
+      console.log(`Successfully registered. Scope is ${reg.scope}`);
+    }).catch((error) => {
+      console.log(`Registering failed ${error}`);
+    });
     return;
   }
   console.log("Service worker not supported in this browser");
